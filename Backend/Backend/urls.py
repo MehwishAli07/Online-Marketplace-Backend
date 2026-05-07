8017d621ff159dbbfda856051be04a84ec1b7b4f
CFmsu <msu-cfontenot68@mcneese.edu>
2026-05-07 11:24:44 -0500

    mirroring more changes

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -1,31 +1,25 @@
-"""
-URL configuration for Backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Online_Marketplace.urls')),
+    path('', views.home, name='home'),
+    #seller dashboard
+    path('seller/', views.seller_page, name='seller'),
+    
+    path('Cart/', views.Cart, name='Cart'),
+    #delte a product 
+
+    path('Checkout/', views.Checkout, name='Checkout'),
+    
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+
+ # alec's script to handoff the product id value specified at the end of the url
+    path('product/<int:product_id>/', views.productRecordHandoff, name='product_detail'), 
+
+    #alec's cart path to salida
+    #path('cart/', views.cart_view, name='cart'),
 ]
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 
