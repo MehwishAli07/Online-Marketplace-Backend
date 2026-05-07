ab8cc42a9591f93ec8648170828e86ac11d3a517
AlecUnkel <aunkel2@mcneese.edu>
2026-05-07 10:29:37 -0500

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    #seller dashboard
+    path('seller/', views.seller_page, name='seller'),
+    
+    #delte a product 
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+
+    # alec's script to handoff the product id value specified at the end of the url
+    path('product/<int:product_id>/', views.productRecordHandoff, name='product_detail'), 
+
+    #alec's cart path to salida
+    #path('cart/', views.cart_view, name='cart'),
+]
+
+
+
