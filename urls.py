1af4a32cbac0d9fb12ba3b2813b26c588ea8d94c
Solida10 <tdaneat110@gmail.com>
2026-05-08 19:49:33 -0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -6,11 +6,16 @@ urlpatterns = [
     #seller dashboard
     path('seller/', views.seller_page, name='seller'),
     
-    path('Cart/', views.Cart, name='Cart'),
-    #delte a product 
+    # Shopping cart page
+    path('Cart/', views.shopping_cart, name='Cart'),
+    path('add/<int:product_id>/', views.addProductCart, name='addProductCart'),
+    path('remove/<int:item_id>/', views.removeFromCart, name='removeFromCart'),
 
+    # Checkout page
     path('Checkout/', views.Checkout, name='Checkout'),
-    
+
+
+     #delte a product 
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 
  # alec's script to handoff the product id value specified at the end of the url
@@ -19,6 +24,9 @@ urlpatterns = [
     #alec's cart path to salida
     #path('cart/', views.cart_view, name='cart'),
 ]
+
+
+
 
 
 
